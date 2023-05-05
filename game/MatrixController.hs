8d560a0732c388ba09a066f71e9b12c10466ab24
Steylor <steylornb@gmail.com>
2023-05-05 16:35:43 -0300

    ajustes no codigo
    fiz ajustes em:
    - nome de algumas funções
    - funções do FallinGrid q marquei como devem ser alteradas
    - novos Data Types
    - ajustes na arquitetura de codigo trocando a matrix de [[Int]] para [[Square]] que satisfaz melhor nossas necessidades.

--- game/MatrixController.hs
+++ game/MatrixController.hs
@@ -1,20 +1,25 @@
+data Move = Left | Right | Rotate | Down | SuperDown
 data Color = Blue | Cyan | Orange | Yellow | Green | Violet | Red deriving Eq
 data Piece = None | LeftL | RightL | Square | Rectangule | LeftS | RigthS | T deriving Eq
 data Falling = Enable | Disable deriving Eq
-
+data Square = (Color,Falling) deriving Eq
 
 -- pronta
-findIndexes :: Int -> [[Int]] -> [(Int, Int)]
+findIndexes :: Int -> [[Square]] -> [(Int, Int)]
 findIndexes s matrix = do
 -- iterate by the rows and columns to get the i and j of the elements
   (i, row) <- zip [0..] matrix
   (j, elem) <- zip [0..] row
   if elem == s then return (i, j) else []
 
--- FALTA MODULARIZAR PARA OS LADOS
-canMove :: [[Int]] -> Bool
-canMove matrix coordinate = if bellow == 2 then False else True
-    where bellow = matrix !! ((fst coordinate) + 1) !! (snd coordinate) 
+-- -- FALTA MODULARIZAR PARA OS LADOS
+-- A DISCUTIR
+-- canMove :: [[Square]] -> Bool
+-- canMove matrix coordinate = if bellow == 2 then False else True
+--     where bellow = matrix !! ((fst coordinate) + 1) !! (snd coordinate) 
+
+--recebe uma grid e uma direção (esq, baixo, dir). retorna se os blocos ativos podem se mover nessa direçao
+canMove :: [[Square]] -> Move -> Bool
 
 -- PARA CONSULTA
 --canFallDownBlock :: [[Int]] -> [(Int,Int)] -> Bool
@@ -23,31 +28,35 @@ canMove matrix coordinate = if bellow == 2 then False else True
 --    if canFallDown matrix (fst x, snd x) then  canFallDownBlock matrix xs
 --    else False
 -- pega a grid, o conjunto de coordenadas ativas e uma direção. retorna se elas podem ir pra uma direçao
-canMoveTetromino :: [[Int]] -> [(Int,Int)] -> Int -> Bool
+canMoveTetromino :: [[Square]] -> [(Int,Int)] -> Int -> Bool
 
 
 -- pronta
-fallTetromino :: [[Int]] -> [(Int,Int)] -> [[Int]]
-fallTetromino :: [[Int]] -> [(Int,Int)] -> [(Int,Int)]
+-- TROCAR PELA MOVE TETROMINO!!
+fallTetromino :: [[Square]] -> [(Int,Int)] -> [[Square]]
+fallTetromino :: [[Square]] -> [(Int,Int)] -> [[Square]]
 fallTetromino matrix [x] = [((fst x + 1), snd x)]
 fallTetromino matrix (x:xs) = ((fst x + 1), snd x) : fallTetromino matrix xs
 
+--move todos os blocos ativos numa direção. presume que eles podem ser movidos
+moveTetromino :: [[Square]] -> Move -> [[Square]]
+
 -- S
 -- Pega uma matriz e retorna a lista de linhas que podem ser limpas
-clearableLines :: [[Int]] -> [Int]
+clearableLines :: [[Square]] -> [Int]
 -- usa canClearLine
 
 --S
 -- pega uma matriz e um índice. retorna se essa linha é clearável ou não
-canClearLine :: [[Int]] -> Int -> Bool
+canClearLine :: [[Square]] -> Int -> Bool
 
 --S
 -- pega uma matriz e uma lista de índices. retorna uma matriz com todos esses índices clearados
-clearTheseLines :: [[Int]] -> [Int] -> [[Int]]
+clearTheseLines :: [[Square]] -> [Int] -> [[Square]]
 
 -- S
 -- pega uma matriz e retorna ela com todas as linhas clearáveis clearadas
-clearMatrix :: [[Int]] -> ([[Int]], Int)
+clearMatrix :: [[Square]] -> ([[Int]], Int)
 -- clearMatrix grid = 
     -- x = clearableLines grid
     -- clearedMatrix = clearTheseLines grid x
@@ -55,7 +64,7 @@ clearMatrix :: [[Int]] -> ([[Int]], Int)
 
 -- L
 -- pega uma matriz e um sentido. retorna essa matriz com os blocos ativos rotacionados pra direita
-rotate :: [[Int]] -> [[Int]]
+rotate :: [[Square]] -> [[Square]]
 
 -- L
 --pega as coordenadas de cada bloco de um tetromino. retorna as coordenadas finais que cada bloco deve ter.
@@ -63,37 +72,29 @@ getRotationEndPositions :: [(Int,Int)] -> [(Int,Int)]
 
 -- E
 --retorna se um conjunto de blocos pode ser colocado na grid.
-canBePut :: [[Int]] -> [(Int, Int)] -> Bool
+canBePut :: [[Square]] -> [(Int, Int)] -> Bool
 
 -- 
 --retorna 1 se pode ser posto com um movimento pra esquerda. 2 com um pra direita. 0 se não pode
-canBePutWithSideMove :: [[Int]] -> [(Int, Int)] -> Int
+canBePutWithSideMove :: [[Square]] -> [(Int, Int)] -> Int
 
 --pega um conjunto de coordenadas. sobe elas até que possa botar elas na matriz, então bota elas na matriz
-raiseUntilAllowed :: [[Int]] -> [(Int, Int)] -> [[Int]]
+raiseUntilAllowed :: [[Square]] -> [(Int, Int)] -> [[Square]]
 
 --retorna true se tem algum bloco acima do limite da grid, false caso contrário
-isGameOver :: [[Int]] -> Bool
+isGameOver :: [[Square]] -> Bool
 
 --usa magia de Gloss para exibir a grid na tela
-showGrid :: [[Int]] -> ????
+showGrid :: [[Square]] -> ????
 
-[0,0,1],[0,2,0]
-001
-020
 
 showGameOver :: ????
 
---recebe uma grid e uma direção (esq, baixo, dir). retorna se os blocos ativos podem se mover nessa direçao
-canMove :: [[Int]] -> Int -> Bool
 
---move todos os blocos ativos numa direção. presume que eles podem ser movidos
-moveActiveBlocks :: [[Int]] -> Int -> [[Int]]
-
-fullFall :: [[Int]] -> [[Int]]
+fullFall :: [[Square]] -> [[Square]]
 
 -- retorna a nova grid, com os blocos ativos derrubados pra baixo.
-forceFall :: [[Int]]
+forceFall :: [[Square]]
 --  showGrid grid
 --  if isGameOver
 --     showGameOver
@@ -101,9 +102,9 @@ forceFall :: [[Int]]
 --      goToNextCycle
 
 -- desativa todos os blocos
-groundBlocks :: [[Int]] -> [[Int]]
+groundBlocks :: [[Square]] -> [[Square]]
 
 getRandomTetromino :: ([(Int, Int)], Int)
 
 -- bota um tetromino aleatório na grid.
-putRandomTetromino :: [[Int]] -> [[Int]]
+putRandomTetromino :: [[Square]] -> [[Square]]
