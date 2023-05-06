4f246b5d5389cb0df6d9b1480e18fa61f47deb8a
Steylor <steylornb@gmail.com>
2023-05-06 15:47:20 -0300

    .

--- game/MatrixController.hs
+++ game/MatrixController.hs
@@ -1,5 +1,5 @@
 data Move = Left | Right | Rotate | Down | SuperDown
-data Color = Blue | Cyan | Orange | Yellow | Green | Violet | Red deriving Eq
+data Color = Black | Blue | Cyan | Orange | Yellow | Green | Violet | Red deriving Eq
 data Piece = LeftL | RightL | Square | Rectangule | LeftS | RigthS | T deriving Eq
 data Active = Enable | Disable | None deriving Eq
 data Square = (Color,Active) deriving Eq
@@ -16,12 +16,11 @@ findActiveIndexes s matrix = do
 -- TO TEST
 --recebe uma grid e uma direção (esq, baixo, dir). retorna se os blocos ativos podem se mover nessa direçao
 canMove :: [[Square]] -> (Int, Int) -> Move -> Bool
-canMove matrix coordinate move = if posMove == Disable then False else True
+canMove matrix coordinate move = if (snd posMove) == Disable then False else True
     where posMove =
         if move == Left then matrix !! (fst coordinate) !! ((snd coordinate) - 1)
         else if move == Right then matrix !! (fst coordinate) !! ((snd coordinate) + 1)
         else if move == Down then matrix !! ((fst coordinate) + 1) !! (snd coordinate)
-
 
 -- TO TEST
 -- pega a grid, o conjunto de coordenadas ativas e uma direção. retorna se elas podem ir pra uma direçao
@@ -31,30 +30,55 @@ canMoveTetromino matrix (x:xs) move =
     if canMove matrix (fst x, snd x) move then canMoveTetromino matrix xs move
     else False
 
+-- magia.
+updateMatrixElement :: [[Square]] -> (Int, Int) -> Square -> [[Square]]
+updateMatrixElement matrix (i, j) newValue =
+  take i matrix ++
+  [take j (matrix !! i) ++ [newValue] ++ drop (j + 1) (matrix !! i)] ++
+  drop (i + 1) matrix
 
--- TO TEST
---move todos os blocos ativos numa direção. presume que eles podem ser movidos
-moveTetromino :: [[Square]] ->[(Int, Int)] Move -> [[Square]]
-moveTetromino matrix [x] move = [posMove]
+
+-- recebe uma posição na matrix e move para a direção desejada
+moveBlock :: [[Square]] -> (Int, Int) -> Move -> [[Square]]
+moveBlock matrix coordinate move =
+    updateMatrixElement auxMatrix coordinate (Black, None) 
+    where
+        posMove =
+            if move == Left then matrix !! (fst coordinate) !! ((snd coordinate) - 1)
+            else if move == Right then matrix !! (fst coordinate) !! ((snd coordinate) + 1)
+            else if move == Down then matrix !! ((fst coordinate) + 1) !! (snd coordinate)
+        -- create an aux matrix that will have the element on the position after move but still have on the initial position to
+        auxMatrix = updateMatrixElement matrix posMove (matrix !! fst coordinate !! snd coordinate)
+
+
+moveTetromino :: [[Square]] -> [(Int, Int)] -> Move -> [[Square]]
+moveTetromino matrix [x] move = moveBlock matrix (fst x, snd x) move 
 moveTetromino matrix (x:xs) move =posMove : moveTetromino matrix xs move
     where posMove =
         if move == Left then ((fst x + 1), snd x)
         else if move == Right then (fst x, (snd x + 1))
         else if move == Down then matrix !! ((fst x + 1), snd x)
 
--- S
+
+
+
+-- TO TEST
 -- Pega uma matriz e retorna a lista de linhas que podem ser limpas
 clearableLines :: [[Square]] -> [Int]
+clearableLines matrix = [i | (row, i) <- zip matrix [0..], canClearLine matrix i]
 
 -- usa canClearLine
 
---S
+-- TO TEST
 -- pega uma matriz e um índice. retorna se essa linha é clearável ou não
 canClearLine :: [[Square]] -> Int -> Bool
+canClearLine matrix rowIndex = allDisable (matrix !! rowIndex)
+    where allDisable xs = all (==Disable) xs
 
---S
+-- TODO
 -- pega uma matriz e uma lista de índices. retorna uma matriz com todos esses índices clearados
 clearTheseLines :: [[Square]] -> [Int] -> [[Square]]
+clearTheseLines matrix lines =
 
 -- S
 -- pega uma matriz e retorna ela com todas as linhas clearáveis clearadas
