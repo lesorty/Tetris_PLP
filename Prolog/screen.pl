0bf1419799891661538300a0059b8d01e7824b41
juliohsu <tnt700900@gmail.com>
2023-05-31 00:07:44 -0300

    julio first's prolog commit

--- Prolog/screen.pl
+++ Prolog/screen.pl
@@ -1,22 +1,28 @@
-% Screen
-
 % -- showGrid, desenha o grid completo na tela
-% showGrid :: [[Square]] -> Int -> Picture
+showGrid([]).
+showGrid([ X | XS ]) :- 
+    drawLine(X),
+    showGrid(XS).
 
-% -- Define a largura e a altura de cada célula do grid
-% cellWidth :: Float
+drawLine(RawLine) :-
+    atom_chars(RawLine, CharsLst),
+    convert(CharsLst, Line),
+    writeln(Line).
 
-% -- a janela do jogo
-% window :: Display
+convert([], "").
+convert([ A | AS ], FinalLine) :-
+    char_to_cell(A, ACell),
+    convert(AS, Line),
+    string_concat(ACell, Line, FinalLine).
 
 % -- define as cores de cada quadrado
-% colorForSquare :: Square -> Color
-
-% -- converte uma coordenada de célula (x, y) em uma posição da tela (x, y)
-% cellToScreen :: (Int, Int) -> (Float, Float)
-
-% -- desenha uma célula na tela, com divisórias
-% drawCell :: (Int, Int) -> Square -> Picture
+char_to_cell('r', "\033[91m██\033[91m"). % red (vermelho)
+char_to_cell('g', "\033[92m██\033[92m"). % green (verde)
+char_to_cell('y', "\033[93m██\033[93m"). % yellow (amarelo)
+char_to_cell('b', "\033[94m██\033[94m"). % blue (azul)
+char_to_cell('m', "\033[95m██\033[95m"). % magenta (roxo)
+char_to_cell('c', "\033[96m██\033[96m"). % cyan (ciano)
+char_to_cell('.', "\033[97m██\033[97m"). % white (branco)
    
 % -- exibe a tela de game over com a pontuação atual
 % showGameOver :: Int -> Picture
@@ -28,6 +34,3 @@ showGrid([]).
 showGrid([H|T]) :-
     showGrid(T),
     showGridLine(H), nl.
-    
-
-
